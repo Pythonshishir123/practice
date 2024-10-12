ef77486909b3c47dca03b6a5569d05b712c6c959
Ubuntu <ubuntu@ip-172-31-37-64.ap-south-1.compute.internal>
2024-10-12 19:21:14 +0000

    first f commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
-# Databas testingouiee
+# Databas testingouijhggd jdghjdg jdhgjdee
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
